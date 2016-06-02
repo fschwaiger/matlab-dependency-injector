cef8b54af654a03b0b033e614671bbfdda403cf1
Florian Schwaiger <fschwaiger@gmail.com>
2016-06-02 21:24:35 +0200

    save dependencies each seperately & input validation

--- Injector.m
+++ Injector.m
@@ -66,11 +66,11 @@ classdef Injector < handle
     %   get   -  Resolve meta.class or config string to a value.
     %
     % See also inputname, meta.class, meta.method, meta.property
-    
+
     % The MIT License (MIT)
-    % 
-    % Copyright (c) 2016 Florian Schwaiger (fschwaiger@gmail.com)
-    % 
+    %
+    % Copyright (c) 2016 Florian Schwaiger
+    %
     % Permission is hereby granted, free of charge, to any person obtaining
     % a copy of this software and associated documentation files (the
     % "Software"), to deal in the Software without restriction, including
@@ -78,10 +78,10 @@ classdef Injector < handle
     % distribute, sublicense, and/or sell copies of the Software, and to
     % permit persons to whom the Software is furnished to do so, subject to
     % the following conditions:
-    % 
+    %
     % The above copyright notice and this permission notice shall be
     % included in all copies or substantial portions of the Software.
-    % 
+    %
     % THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
     % EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
     % MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -90,11 +90,11 @@ classdef Injector < handle
     % ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
     % CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     % SOFTWARE.
-    
+
     properties (Access = private)
         Resolved@containers.Map
     end
-    
+
     methods
         function self = Injector(varargin)
             % New injector instance with known dependencies as params.
@@ -111,11 +111,11 @@ classdef Injector < handle
             %
             % See also inputname
             self.Resolved = containers.Map();
-            
+
             names = arrayfun(@inputname, 1:nargin, 'Uniform', false);
             self.saveAsResolved(names, varargin);
         end
-        
+
         function self = with(self, varargin)
             % Add more known dependencies on-the-fly.
             %
@@ -133,7 +133,7 @@ classdef Injector < handle
             names = arrayfun(@inputname, 2:nargin, 'Uniform', false);
             self.saveAsResolved(names, varargin);
         end
-        
+
         function value = get(self, varargin)
             % Resolves a new value and saves it.
             %
@@ -146,7 +146,7 @@ classdef Injector < handle
             % returns its handle / value. Classes will then be remembered as
             % their truncated ClassName, methods and redirects by their names.
             [scope, name, path] = resolveArguments(varargin);
-            
+
             if strcmp(name, 'scope')
                 % current package name, empty if outside any package
                 value = scope;
@@ -168,14 +168,14 @@ classdef Injector < handle
             end
         end
     end
-    
+
     methods (Access = private)
         function saveAsResolved(self, names, values)
             % Saves the given values as resolved by the given names.
             index = 1;
             while index <= length(names)
                 name = names{index};
-                
+
                 if isempty(name)
                     name = values{index};
                     value = values{index + 1};
@@ -184,23 +184,27 @@ classdef Injector < handle
                     value = values{index};
                     index = index + 1;
                 end
-                
+
                 self.Resolved(name) = value;
             end
         end
-        
+
         function value = resolveMethodValue(self, scope, name, path)
             % Executes a function handle to resolve the specified value.
             methodInfo = resolveInjectorMethod(scope, name, path);
-                
-            dependencies = cellfun( ...
-                @(name) self.get(methodInfo.Scope, name), ...
-                methodInfo.Parameters, 'Uniform', false);
-            self.saveAsResolved(methodInfo.Parameters, dependencies);
 
-            if ~isempty(dependencies) && strcmp(methodInfo.Parameters{end}, 'varargin')                    
+            dependencies = cell(size(methodInfo.Parameters));
+            for index = 1:numel(dependencies)
+                name = methodInfo.Parameters{index};
+                value = self.get(methodInfo.Scope, name);
+
+                dependencies{index} = value;
+                self.Resolved(name) = value;
+            end
+
+            if ~isempty(dependencies) && strcmp(methodInfo.Parameters{end}, 'varargin')
                 value = methodInfo.Function(dependencies{1:end-1}, dependencies{end}{:});
-            else 
+            else
                 value = methodInfo.Function(dependencies{:});
             end
         end
@@ -210,10 +214,17 @@ end
 function [scope, name, path] = resolveArguments(arguments)
     % Resolves the varargin to scope, name and path.
     if isscalar(arguments)
-        if isa(arguments{1}, 'meta.class')
-            path = arguments{1}.Name;
+        pathOrClass = arguments{1};
+        if isa(pathOrClass, 'meta.class')
+            assert(~isempty(pathOrClass), ...
+                'Injector:InvalidArgument', 'Missing meta class provided.');
+
+            path = pathOrClass.Name;
             [scope, name] = splitPath(path);
         else
+            assert(ischar(pathOrClass), ...
+                'Injector:InvalidArgument', 'Argument is not a vaid path identifier.');
+
             [scope, name, path] = normalizePath('', arguments{1});
         end
     else
@@ -237,13 +248,13 @@ function methodInfo = resolveInjectorMethod(scope, name, path)
     %   Scope       -  String referring to the package scope of the method.
     %
     % See also resolveInjectorConfigMethod, resolveClassContructorMethod
-    
+
     % internal cache for resolved function handles
     persistent methodCache
     if isempty(methodCache)
         methodCache = containers.Map();
     end
-    
+
     if isKey(methodCache, path)
         methodInfo = methodCache(path);
     else
@@ -264,7 +275,7 @@ function methodInfo = resolveInjectorConfigMethod(scope, name)
     % See also resolveInjectorMethod
     configPath = joinPath(scope, 'InjectorConfig');
     configClass = meta.class.fromName(configPath);
-    
+
     configProperty = findobj(configClass.PropertyList, 'Name', name);
     if ~isempty(configProperty)
         path = configProperty.DefaultValue;
@@ -272,15 +283,15 @@ function methodInfo = resolveInjectorConfigMethod(scope, name)
         methodInfo = resolveInjectorMethod(scope, name, path);
         return
     end
-    
+
     configMethod = findobj(configClass.MethodList, 'Name', name);
-    if ~isempty(configMethod)    
+    if ~isempty(configMethod)
         methodInfo.Function = str2func([configPath '.' name]);
         methodInfo.Parameters = configMethod.InputNames;
         methodInfo.Scope = scope;
         return
     end
-    
+
     error('No field or method ''%s'' in class ''%s''.', name, configPath);
 end
 
@@ -317,7 +328,7 @@ function [scope, name, path] = normalizePath(scope, name)
         path = joinPath(scope, name);
     end
 end
-   
+
 function [scope, name] = splitPath(path)
     % Splits 'scope.test.class' into 'scope.test' and 'class'
     for dot = length(path) : -1 : 1
@@ -325,7 +336,7 @@ function [scope, name] = splitPath(path)
             break
         end
     end
-    
+
     scope = path(1:dot-1);
     name = path(1+dot:end);
 end
